0c74300810817cf1e55a5cfb91a5b622a9c2e4d9
Priya <priyakavee25@gmail.com>
2026-07-05 16:49:26 +0530

    Added modules in master branch

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "env" {
+  type        = string
+  description = "What Env we are creating (dev, qa, production)"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "VPC CIDR"
+}
+
+variable "subnet_cidr" {
+  type        = string
+  description = "SUBNET CIDR"
+}
+
+variable "subnet_az" {
+  type        = string
+  description = "AZ of the Subnet"
+}
