1800e94690c3f89648acdebf0303be5e8673962d
Priya <priyakavee25@gmail.com>
2026-07-05 19:28:08 +0530

    Changes made in subnet id

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+  subnet_id     = module.vpc.subnet_id
   count         = var.ec2_count
 
   tags = {
